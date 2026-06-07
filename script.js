a253f2113214030e5788f23588bda32f558f408a
Sudeep H A <sudeephasudeepha@gmail.com>
2026-06-07 20:31:12 +0530

    Update script.js

--- script.js
+++ script.js
@@ -154,7 +154,7 @@ function startConfetti() {
             ctx.translate(heart.x, heart.y);
 
             // Make heart slightly taller
-            ctx.scale(1, 1.15);
+            ctx.scale(1, 1.3);
 
             // Draw actual emoji
             ctx.fillText("❤️", 0, 0);
